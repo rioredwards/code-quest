e4dfc901d7744fb5dd8857c4ac41bca3de99c65d
Rio Edwards <rioredwards@gmail.com>
2023-06-27 13:36:10 -0700

    Honing in spin with different variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import "./App.css";
-import Reel from "./Reel";
 import ReelTwo from "./ReelTwo";
 
+const choices = [
+  "0 React",
+  "1 Typescript",
+  "2 Javascript",
+  "3 Python",
+  "4 Rust",
+  "5 Go",
+  "6 C++",
+  "7 C",
+  "8 Java",
+  "9 C#",
+];
+
 function App() {
+  const [spinning, setSpinning] = useState(false);
+
   return (
     <div className="App">
-      <ReelTwo />
+      <button onClick={() => setSpinning(true)}>Spin</button>
+      <button onClick={() => setSpinning(false)}>Stop</button>
+      <ReelTwo choices={choices} delay={0} spinning={spinning} />
     </div>
   );
 }
--- src/ReelTwo.tsx
+++ src/ReelTwo.tsx
@@ -1,5 +1,59 @@
-const ReelTwo: React.FC = () => {
-  return <div>ReelTwo</div>;
+import "./ReelTwo.css";
+import { Variants, motion } from "framer-motion";
+
+interface ReelTwoProps {
+  choices: string[];
+  delay: number;
+  spinning: boolean;
+}
+
+const repeat = (arr: any[], n: number) => [].concat(...Array(n).fill(arr));
+
+const spinVariants = {
+  idle: (i: number) => ({
+    y: `-${i * 2}rem`,
+    transition: {
+      duration: 0,
+    },
+  }),
+  idleSpin: (i: number) => ({
+    y: [`-${i * 2}rem`, `0rem`],
+    transition: {
+      duration: 1,
+      repeat: Infinity,
+      ease: "linear",
+    },
+  }),
+  stopSpin: (i: number) => ({
+    y: `0rem`,
+    transition: {
+      duration: 2,
+      ease: "easeOut",
+    },
+  }),
+};
+
+const ReelTwo: React.FC<ReelTwoProps> = ({ choices, spinning }) => {
+  const doubledChoices = repeat(choices, 2);
+
+  return (
+    <div className="reel-container">
+      <div className="reel-window">
+        <motion.ul
+          custom={choices.length}
+          variants={spinVariants}
+          initial="idle"
+          animate={spinning ? "idleSpin" : "stopSpin"}
+          className="reel">
+          {doubledChoices.map((choice, i) => (
+            <li className="choice" key={i}>
+              {choice}
+            </li>
+          ))}
+        </motion.ul>
+      </div>
+    </div>
+  );
 };
 
 export default ReelTwo;
