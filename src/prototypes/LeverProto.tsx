65f122d4cee90148e92bbc03e6a6a96d6296c3a9
Rio Edwards <rioredwards@gmail.com>
2023-07-06 10:55:13 -0700

    Add skeleton for pull function

--- src/prototypes/LeverProto.tsx
+++ src/prototypes/LeverProto.tsx
@@ -2,22 +2,31 @@ import "./LeverProto.css";
 import { motion, useMotionValue, useTransform } from "framer-motion";
 
 interface LeverProtoProps {}
+const pullThreshold = 100;
+let pulled = false;
 
 const LeverProto: React.FC<LeverProtoProps> = () => {
   const dragYPos = useMotionValue(0);
-  const dragXPos = useTransform(
-    dragYPos,
-    [0, 35, 70, 105, 140],
-    [0, 15, 30, 15, 0]
-  );
+  const dragXPos = useTransform(dragYPos, [0, 70, 140], [0, 30, 0]);
   const leverYPos = useTransform(dragYPos, [0, 140], [0, 10]);
   const rotationAngle = useTransform(dragYPos, [0, 140], [-45, 45]);
+
+  dragYPos.on("change", (yValue) => {
+    if (!pulled && yValue > pullThreshold) {
+      onPull();
+      pulled = true;
+    }
+  });
+
+  const onPull = () => {
+    console.log("pull");
+  };
 
   return (
     <div className="lever-container">
       <motion.div className="lever-base" />
       <motion.div
-        style={{ y: leverYPos, rotate: rotationAngle, translateX: 10 }}
+        style={{ y: leverYPos, rotate: rotationAngle, translateX: "1vh" }}
         className="lever-handle"
       />
       <motion.div
