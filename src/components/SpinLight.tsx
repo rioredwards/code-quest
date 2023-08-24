73b2babcde31a555f00e688c87acdd6b28e758d0
Rio Edwards <rioredwards@gmail.com>
2023-08-24 14:17:53 -0700

    Change spin-lights to use seperate image for glass texture and seperate tag for color. This simplifies changing colors

--- src/components/SpinLight.tsx
+++ src/components/SpinLight.tsx
@@ -11,33 +11,20 @@ const SpinLight: React.FC<SpinLightProps> = ({
   onClickSpinLight,
 }) => {
   const color = calcColorForSpinMode(spinState);
-  const offset = calcOffsetPercentForColor(color);
 
   return (
     <div className="spin-light-container">
-      <div
-        onClick={() => onClickSpinLight(spinState)}
-        className="spin-light"
-        style={{ backgroundPositionX: offset }}
-      />
+      <div className={`spin-light-color ${color}`}>
+        <div
+          onClick={() => onClickSpinLight(spinState)}
+          className="spin-light-glass"
+        />
+      </div>
     </div>
   );
 };
 
 export default SpinLight;
-
-function calcOffsetPercentForColor(color: string): string {
-  switch (color) {
-    case "green":
-      return "0%";
-    case "yellow":
-      return "50%";
-    case "red":
-      return "100%";
-    default:
-      return "0%";
-  }
-}
 
 function calcColorForSpinMode(state: SpinState) {
   switch (state) {
