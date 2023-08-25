a4e480c0d83ba952a8d2cc0dd2ae9c0f32cab3ce
Rio Edwards <rioredwards@gmail.com>
2023-08-25 12:28:36 -0700

    Remove prototypes dir

--- src/prototypes/Example.tsx
+++
@@ -1,9 +0,0 @@
-import "./Example.css";
-
-interface ExampleProps {
-  children: React.ReactNode;
-}
-
-export const Example: React.FC<ExampleProps> = ({ children }) => {
-  return <div className="example-container">{children}</div>;
-};
