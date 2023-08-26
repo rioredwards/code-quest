5ea47aa4ecfc0e4e383ebb6808c7d2a9d90cd3fd
Rio Edwards <rioredwards@gmail.com>
2023-08-26 09:24:12 -0700

    Remove unnecessary counter slice from store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import reelsReducer from "./reels/reelsSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     reels: reelsReducer,
   },
 });
