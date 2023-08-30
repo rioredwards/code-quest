6cfca07f9125eed552cd779b68a725c0b626716c
Rio Edwards <rioredwards@gmail.com>
2023-08-29 17:03:30 -0700

    Start refining choice details (compatibility, sentences, etc...) and adding more tasks

--- src/data/choices/taskChoices.ts
+++ src/data/choices/taskChoices.ts
@@ -1,124 +1,308 @@
 import { Choice } from "../../types";
+import { TimeChoiceIdx } from "./timeChoices";
+
+const {
+  THIRTY_MIN,
+  FORTY_FIVE_MIN,
+  ONE_HOUR,
+  NINETY_MIN,
+  TWO_HOURS,
+  THREE_HOURS,
+  FOUR_HOURS,
+  FIVE_HOURS,
+  INFINITY,
+} = TimeChoiceIdx;
+
+const leetCodeEasy: TimeChoiceIdx[] = [
+  THIRTY_MIN,
+  FORTY_FIVE_MIN,
+  ONE_HOUR,
+  NINETY_MIN,
+  INFINITY,
+];
+const leetCodeMediumish: TimeChoiceIdx[] = [
+  ONE_HOUR,
+  NINETY_MIN,
+  TWO_HOURS,
+  INFINITY,
+];
+const leetCodeMedium: TimeChoiceIdx[] = [
+  NINETY_MIN,
+  TWO_HOURS,
+  THREE_HOURS,
+  INFINITY,
+];
+
+const backendEasy: TimeChoiceIdx[] = [TWO_HOURS, THREE_HOURS, INFINITY];
+const backendMedium: TimeChoiceIdx[] = [THREE_HOURS, FOUR_HOURS, INFINITY];
+const backendHard: TimeChoiceIdx[] = [FOUR_HOURS, FIVE_HOURS, INFINITY];
+
+const frontendEasy: TimeChoiceIdx[] = [TWO_HOURS, THREE_HOURS, INFINITY];
+const frontendMedium: TimeChoiceIdx[] = [THREE_HOURS, FOUR_HOURS, INFINITY];
+const frontendHard: TimeChoiceIdx[] = [FOUR_HOURS, FIVE_HOURS, INFINITY];
 
 export const taskChoices: Choice[] = [
   {
     id: 0,
-    name: "Two Sum",
+    name: "Two Sum", // Leetcode -> Easy
     sentenceName: "Two Sum",
     compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
+      TYPE: [0],
       TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
+      TIME: leetCodeEasy,
     },
   },
   {
     id: 1,
-    name: "Reverse String",
-    sentenceName: "Reverse String",
+    name: "Contains Duplicate", // Leetcode -> Easy
+    sentenceName: "Contains Duplicate",
     compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
+      TYPE: [0],
       TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
+      TIME: leetCodeEasy,
     },
   },
   {
     id: 2,
-    name: "Contains Duplicate",
-    sentenceName: "Contains Duplicate",
+    name: "Valid Anagram", // Leetcode -> Easy
+    sentenceName: "Valid Anagram",
     compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
+      TYPE: [0],
       TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
+      TIME: leetCodeEasy,
     },
   },
   {
     id: 3,
-    name: "Valid Anagram",
-    sentenceName: "Valid Anagram",
-    compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
-      TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
-    },
-  },
-  {
-    id: 4,
-    name: "Group Anagrams",
-    sentenceName: "Group Anagrams",
-    compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
-      TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
-    },
-  },
-  {
-    id: 5,
-    name: "Top K Frequent Elements",
-    sentenceName: "Top K Frequent Elements",
-    compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
-      TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
-    },
-  },
-  {
-    id: 6,
-    name: "Valid Palindrome",
+    name: "Valid Palindrome", // Leetcode -> Easy
     sentenceName: "Valid Palindrome",
     compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
+      TYPE: [0],
       TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
-    },
-  },
-  {
-    id: 7,
-    name: "Container With Most Water",
-    sentenceName: "Container With Most Water",
-    compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
-      TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
+      TIME: leetCodeEasy,
     },
   },
   {
     id: 8,
-    name: "Best Time to Buy And Sell Stock",
-    sentenceName: "Best Time to Buy And Sell Stock",
+    name: "Merge Two Lists", // Leetcode -> Easy (medium-ish)
+    sentenceName: "Merge Two Sorted Lists",
     compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
+      TYPE: [0],
       TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
+      TIME: leetCodeMediumish,
     },
   },
   {
     id: 9,
-    name: "Longest Substring Without Repeating Characters",
+    name: "Reverse Linked List", // Leetcode -> Easy (medium-ish)
+    sentenceName: "Reverse Linked List",
+    compatibleWith: {
+      TYPE: [0],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: leetCodeMediumish,
+    },
+  },
+  {
+    id: 4,
+    name: "Top K Frequent Elements", // Leetcode -> Medium
+    sentenceName: "Top K Frequent Elements",
+    compatibleWith: {
+      TYPE: [0],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: leetCodeMedium,
+    },
+  },
+  {
+    id: 5,
+    name: "Container With Most Water", // Leetcode -> Medium
+    sentenceName: "Container With Most Water",
+    compatibleWith: {
+      TYPE: [0],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: leetCodeMedium,
+    },
+  },
+  {
+    id: 6,
+    name: "Longest Substring", // Leetcode -> Medium
     sentenceName: "Longest Substring Without Repeating Characters",
     compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
+      TYPE: [0],
       TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
+      TIME: leetCodeMedium,
     },
   },
   {
     id: 10,
-    name: "Valid Parentheses",
-    sentenceName: "Valid Parentheses",
+    name: "Rotated Sorted Array", // Leetcode -> Medium
+    sentenceName: "Find Minimum in Rotated Sorted Array",
     compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
+      TYPE: [0],
       TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
+      TIME: leetCodeMedium,
     },
   },
   {
     id: 11,
-    name: "Merge Two Sorted Lists",
-    sentenceName: "Merge Two Sorted Lists",
+    name: "Todos API", // Backend -> Easy
+    sentenceName: "CRUD API for Todos",
     compatibleWith: {
-      TYPE: [0, 1, 2, 3, 4, 5],
+      TYPE: [1],
       TECH: [0, 1, 2, 3, 4, 5],
-      TIME: [0, 1, 2, 3, 4, 5],
+      TIME: backendEasy,
+    },
+  },
+  {
+    id: 12,
+    name: "Blog Posts API", // Backend -> Easy
+    sentenceName: "CRUD API for Blog Posts",
+    compatibleWith: {
+      TYPE: [1],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: backendEasy,
+    },
+  },
+  {
+    id: 13,
+    name: "Pokemon List", // Backend -> easy
+    sentenceName:
+      "proxy server for the the PokeAPI which returns a list of pokemon",
+    compatibleWith: {
+      TYPE: [1],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: backendEasy,
+    },
+  },
+  {
+    id: 14,
+    name: "Movie List", // Backend -> easy
+    sentenceName:
+      "proxy server for the TheMovieDB API which returns a list of movies",
+    compatibleWith: {
+      TYPE: [1],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: backendEasy,
+    },
+  },
+  {
+    id: 15,
+    name: "OpenAI", // Backend -> Medium
+    sentenceName:
+      "proxy server for the OpenAI API to return answers to questions",
+    compatibleWith: {
+      TYPE: [1],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: backendMedium,
+    },
+  },
+  {
+    id: 16,
+    name: "Auth", // Backend -> Medium
+    sentenceName: "API with Authentication, Authorization and Protected Routes",
+    compatibleWith: {
+      TYPE: [1],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: backendMedium,
+    },
+  },
+  {
+    id: 17,
+    name: "Bad Words", // Backend -> Medium
+    sentenceName: "API that filters out bad words from a string",
+    compatibleWith: {
+      TYPE: [1],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: backendMedium,
+    },
+  },
+  {
+    id: 18,
+    name: "E-Commerce", // Backend -> Hard
+    sentenceName: "API for an E-Commerce site with products and orders",
+    compatibleWith: {
+      TYPE: [1],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: backendHard,
+    },
+  },
+  {
+    id: 18,
+    name: "GraphQL Space", // Backend -> Hard
+    sentenceName: "GraphQL API for celestial objects in outer space",
+    compatibleWith: {
+      TYPE: [1],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: backendHard,
+    },
+  },
+  {
+    id: 19,
+    name: "Landing Page", // Frontend -> Easy
+    sentenceName: "Landing Page",
+    compatibleWith: {
+      TYPE: [2],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: frontendEasy,
+    },
+  },
+  {
+    id: 19,
+    name: "Photo Gallery", // Frontend -> Easy
+    sentenceName: "Responsive Photo Gallery",
+    compatibleWith: {
+      TYPE: [2],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: frontendEasy,
+    },
+  },
+  {
+    id: 19,
+    name: "Character Creator", // Frontend -> Medium
+    sentenceName: "Character Creator",
+    compatibleWith: {
+      TYPE: [2],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: frontendMedium,
+    },
+  },
+  {
+    id: 19,
+    name: "Photo Carousel", // Frontend -> Medium
+    sentenceName: "Photo Carousel with infinite scroll",
+    compatibleWith: {
+      TYPE: [2],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: frontendMedium,
+    },
+  },
+  {
+    id: 19,
+    name: "Calculator", // Frontend -> Medium
+    sentenceName: "Calculator with basic arithmetic operations",
+    compatibleWith: {
+      TYPE: [2],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: frontendMedium,
+    },
+  },
+  {
+    id: 19,
+    name: "Tic-Tac-Toe", // Frontend -> Medium
+    sentenceName: "Tic-Tac-Toe game",
+    compatibleWith: {
+      TYPE: [2],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: frontendMedium,
+    },
+  },
+  {
+    id: 19,
+    name: "Admin Dashboard", // Frontend -> Hard
+    sentenceName: "Admin Dashboard",
+    compatibleWith: {
+      TYPE: [2],
+      TECH: [0, 1, 2, 3, 4, 5],
+      TIME: frontendHard,
     },
   },
 ];
--- src/data/choices/timeChoices.ts
+++ src/data/choices/timeChoices.ts
@@ -1,10 +1,22 @@
 import { Choice } from "../../types";
 
+export enum TimeChoiceIdx {
+  THIRTY_MIN = 0,
+  FORTY_FIVE_MIN = 1,
+  ONE_HOUR = 2,
+  NINETY_MIN = 3,
+  TWO_HOURS = 4,
+  THREE_HOURS = 5,
+  FOUR_HOURS = 6,
+  FIVE_HOURS = 7,
+  INFINITY = 8,
+}
+
 export const timeChoices: Choice[] = [
   {
     id: 0,
-    name: "1 Hour",
-    sentenceName: "1 Hour",
+    name: "30 Min",
+    sentenceName: "30 Minutes",
     compatibleWith: {
       TYPE: [0, 1, 2, 3, 4, 5],
       TASK: [0, 1, 2, 3, 4, 5],
@@ -13,6 +25,36 @@ export const timeChoices: Choice[] = [
   },
   {
     id: 1,
+    name: "45 Min",
+    sentenceName: "45 Minutes",
+    compatibleWith: {
+      TECH: [0, 1, 2, 3, 4, 5],
+      TASK: [0, 1, 2, 3, 4, 5],
+      TYPE: [0, 1, 2, 3, 4, 5],
+    },
+  },
+  {
+    id: 2,
+    name: "1 Hour",
+    sentenceName: "1 Hour",
+    compatibleWith: {
+      TECH: [0, 1, 2, 3, 4, 5],
+      TASK: [0, 1, 2, 3, 4, 5],
+      TYPE: [0, 1, 2, 3, 4, 5],
+    },
+  },
+  {
+    id: 3,
+    name: "90 Min",
+    sentenceName: "90 Minutes",
+    compatibleWith: {
+      TECH: [0, 1, 2, 3, 4, 5],
+      TASK: [0, 1, 2, 3, 4, 5],
+      TYPE: [0, 1, 2, 3, 4, 5],
+    },
+  },
+  {
+    id: 4,
     name: "2 Hours",
     sentenceName: "2 Hours",
     compatibleWith: {
@@ -22,7 +64,7 @@ export const timeChoices: Choice[] = [
     },
   },
   {
-    id: 2,
+    id: 5,
     name: "3 Hours",
     sentenceName: "3 Hours",
     compatibleWith: {
@@ -32,7 +74,7 @@ export const timeChoices: Choice[] = [
     },
   },
   {
-    id: 3,
+    id: 6,
     name: "4 Hours",
     sentenceName: "4 Hours",
     compatibleWith: {
@@ -42,7 +84,7 @@ export const timeChoices: Choice[] = [
     },
   },
   {
-    id: 4,
+    id: 7,
     name: "5 Hours",
     sentenceName: "5 Hours",
     compatibleWith: {
@@ -52,8 +94,8 @@ export const timeChoices: Choice[] = [
     },
   },
   {
-    id: 5,
-    name: "Unlimited",
+    id: 8,
+    name: "∞",
     sentenceName: "Unlimited Time",
     compatibleWith: {
       TECH: [0, 1, 2, 3, 4, 5],
