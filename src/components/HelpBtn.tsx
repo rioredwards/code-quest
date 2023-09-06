335f54f1bd3048e237b4cbafe1d16503dc48e38b
Rio Edwards <rioredwards@gmail.com>
2023-09-06 16:57:02 -0700

    Make menu button colors more better

--- src/components/HelpBtn.tsx
+++ src/components/HelpBtn.tsx
@@ -1,18 +1,22 @@
-import { useAppDispatch } from "../store/hooks";
+import { selectHelpState } from "../store/help/helpSlice";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
 import "./HelpBtn.css";
 
 interface Props {}
 
 const HelpBtn: React.FC<Props> = () => {
   const dispatch = useAppDispatch();
+  const menuIsOpen = useAppSelector(selectHelpState).helpMenuIsOpen;
 
   function onClickHelpBtn() {
     dispatch({ type: "help/helpBtnClicked" });
   }
 
   return (
-    <button onClick={onClickHelpBtn} className="help-btn">
-      ?
+    <button
+      onClick={onClickHelpBtn}
+      className={`help-btn ${menuIsOpen ? "menu-open" : ""}`}>
+      {menuIsOpen ? "X" : "?"}
     </button>
   );
 };
