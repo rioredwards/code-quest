957bba1a62b1889ea7bb153cf0310406322c5889
Rio Edwards <rioredwards@gmail.com>
2023-09-07 14:10:01 -0700

    Add brand icons/links to about summary

--- src/components/HelpMenu.tsx
+++ src/components/HelpMenu.tsx
@@ -25,29 +25,51 @@ const HelpMenu: React.FC<Props> = () => {
       )}
       {activeMenu === "ABOUT" && (
         <p className="about-summary left-aligned">
-          <span>Hello 👋</span>
           <div className="custom-line-break" />
           <span>
-            My name is <strong>Rio Edwards</strong>, the creator of this app. My
-            goal here was to create a fun and interactive way to generate
-            pseudo-random coding challenges. I hope you enjoy it and find it
-            useful! 😊
+            My name is <strong>Rio Edwards</strong>. My goal here was to create
+            a fun and interactive way to generate pseudo-random coding
+            challenges. I hope you enjoy it and find it useful!
             <div className="custom-line-break" />
             <div className="custom-line-break" />
-            p.s. I'm currently
+            I'm currently
             <strong> looking for a full-time job</strong> as a software
             developer! If you are interested in hiring me, or just want to say
-            hi, please reach out using the links below. 👇
+            hi, please reach out using the links below.
             <div className="custom-line-break" />
           </span>
           <div className="links">
             <a
+              className="link"
+              href="https://github.com/rioredwards/code-challenge-generator"
+              target="_blank"
+              rel="noreferrer">
+              <FontAwesomeIcon
+                className="icon github"
+                size="2x"
+                icon={icon({ name: "github", style: "brands" })}
+              />
+            </a>
+            <a
+              className="link"
               href="https://www.linkedin.com/in/rio-edwards/"
               target="_blank"
               rel="noreferrer">
               <FontAwesomeIcon
+                className="icon linkedin"
                 size="2x"
                 icon={icon({ name: "linkedin", style: "brands" })}
+              />
+            </a>
+            <a
+              className="link"
+              href="https://youtube.com/@rioredwards?si=Z95LbMxsy0lowObz"
+              target="_blank"
+              rel="noreferrer">
+              <FontAwesomeIcon
+                className="icon youtube"
+                size="2x"
+                icon={icon({ name: "youtube", style: "brands" })}
               />
             </a>
           </div>
